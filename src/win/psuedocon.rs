297ec155c0c9d9b53087fc50e2e0e499643e11f3
MBanucu <michael.banucu@googlemail.com>
2026-02-11 16:35:40 +0100

    fix: correct typo in pseudoconsole constant name
    The constant PSUEDOCONSOLE_INHERIT_CURSOR was misspelled and has been
    corrected to PSEUDOCONSOLE_INHERIT_CURSOR to match the proper spelling.

--- src/win/psuedocon.rs
+++ src/win/psuedocon.rs
@@ -24,7 +24,7 @@ use winapi::um::winnt::HANDLE;
 
 pub type HPCON = HANDLE;
 
-pub const PSUEDOCONSOLE_INHERIT_CURSOR: DWORD = 0x1;
+pub const PSEUDOCONSOLE_INHERIT_CURSOR: DWORD = 0x1;
 pub const PSEUDOCONSOLE_RESIZE_QUIRK: DWORD = 0x2;
 pub const PSEUDOCONSOLE_WIN32_INPUT_MODE: DWORD = 0x4;
 #[allow(dead_code)]
