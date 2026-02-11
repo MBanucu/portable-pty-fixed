03d758b7a9dd952e278971fc2eeac8c28b222656
MBanucu <michael.banucu@googlemail.com>
2026-02-11 18:42:36 +0100

    perf: remove unnecessary sleep in bash test

--- tests/test_bash.rs
+++ tests/test_bash.rs
@@ -83,7 +83,7 @@ mod tests {
             writer.write_all(b"echo hello").unwrap();
             writer.write_all(NEWLINE).unwrap();
 
-            thread::sleep(Duration::from_millis(500));
+            // thread::sleep(Duration::from_millis(500));
 
             // Send exit
             writer.write_all(b"exit").unwrap();
